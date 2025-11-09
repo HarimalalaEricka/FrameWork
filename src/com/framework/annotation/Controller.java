80faa120ef97248c0efb09d386ae033a837bea46
Nam🐼 <harimalalaerickarandria@gmail.com>
2025-11-09 23:57:42 +0300

    q

--- src/com/framework/annotation/Controller.java
+++
@@ -1,9 +0,0 @@
-package com.framework.annotation;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME) 
-@Target(ElementType.TYPE)          
-public @interface Controller {
-    String valeur() default "Controller";
-}
